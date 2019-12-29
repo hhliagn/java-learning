c2fb1e531aa905ff2b31831fa5d123e43d4775f9
lianghenghui <lianghenghui@ice-point.com.cn>
2019-12-29 10:48:23 +0800

    001 empty project

---
+++ src/test/java/com/javalearning/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
