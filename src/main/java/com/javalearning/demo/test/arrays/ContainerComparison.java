1cc8749501882928214f022d2a1caee04b748c51
lianghenghui <lianghenghui@ice-point.com.cn>
2020-01-16 18:02:28 +0800

    20116 lp

---
+++ src/main/java/com/javalearning/demo/test/arrays/ContainerComparison.java
@@ -0,0 +1,13 @@
+package com.javalearning.demo.test.arrays;
+
+class BerylliumSphere{
+    private static long counter = 0;
+    private final long id = counter ++;
+
+    @Override
+    public String toString() {
+        return "Sphere " + id;
+    }
+}
+public class ContainerComparison {
+}
