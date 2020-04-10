403ee248c671005f9720d780f0af4cbc0c776e0d
lianghenghui <lianghenghui@ice-point.com.cn>
2020-04-10 18:11:30 +0800

    add for linkedlist

--- src/main/java/com/javalearning/demo/leetcode/linkedlist/test/MyLinkedList.java
+++ src/main/java/com/javalearning/demo/leetcode/linkedlist/test/MyLinkedList.java
@@ -1,78 +1,67 @@
-package com.javalearning.demo.leetcode.linkedlist;
+package com.javalearning.demo.leetcode.linkedlist.test;
 
 class Node{
     int val;
-    Node next;
     Node prev;
-    Node(int val){
-        this.val = val;
+    Node next;
+    Node(int x){
+        val = x;
     }
 }
 public class MyLinkedList {
-
-    Node head;
-    Node tail;
+    Node head, tail;
     int size;
 
     public MyLinkedList(){
         head = new Node(0);
         tail = new Node(0);
-        //这两步不能省
         head.next = tail;
         tail.prev = head;
         size = 0;
     }
 
-    public int get(int index) {
-        if (index < 0 || index >= size) return -1;
-        Node cur = head;
-        if (index < size-index){
+    public int get(int index){
+        if (index<0 || index>= size) return -1;
+        Node cur;
+        if (index<size-index){
+            cur = head;
             for (int i = 0; i < index+1; i++) {
                 cur = cur.next;
             }
         }else {
             cur = tail;
-            for (int i = 0; i < size - index; i++) {
+            for(int i = 0;i< size-index;i++){
                 cur = cur.prev;
             }
         }
         return cur.val;
     }
 
-    public void addAtHead(int val) {
+    public void addAtHead(int val){
         Node pred = head, succ = head.next;
         Node toAdd = new Node(val);
-
+        toAdd.next= succ;
         toAdd.prev = pred;
-        toAdd.next = succ;
         pred.next = toAdd;
         succ.prev = toAdd;
-
         size++;
     }
 
-    /** Append a node of value val to the last element of the linked list. */
-    public void addAtTail(int val) {
+    public void addAtTail(int val){
         Node pred = tail.prev, succ = tail;
         Node toAdd = new Node(val);
-
+        toAdd.next= succ;
         toAdd.prev = pred;
-        toAdd.next = succ;
         pred.next = toAdd;
         succ.prev = toAdd;
-
         size++;
     }
 
-    public void addAtIndex(int index, int val) {
-        if (index>size){
-            return;
-        }
-        if (index<0){
-            index = 0;
-        }
+    public void addAtIndex(int index,int val){
+        if (index<0) index=0;
+        if (index>size) return;
         Node pred, succ;
-        if (index < size - index){
+        if (index<size-index){
             pred = head;
             for (int i = 0; i < index; i++) {
                 pred = pred.next;
@@ -80,29 +69,27 @@ public class MyLinkedList {
             succ = pred.next;
         }else {
             succ = tail;
-            for (int i = 0; i < size-index; i++) {
+            for(int i = 0;i<size-index;i++){
                 succ = succ.prev;
             }
             pred = succ.prev;
         }
 
         Node toAdd = new Node(val);
-        toAdd.next = succ;
+        toAdd.next= succ;
         toAdd.prev = pred;
         pred.next = toAdd;
         succ.prev = toAdd;
-
         size++;
     }
 
-    /** Delete the index-th node in the linked list, if the index is valid. */
-    public void deleteAtIndex(int index) {
-        if (index <0 || index>=size){
+    public void deleteAtIndex(int index){
+        if (index<0 || index>= size){
             return;
         }
 
         Node pred, succ;
-        if (index<size - index){
+        if (index<size-index){
             pred = head;
             for (int i = 0; i < index; i++) {
                 pred = pred.next;
@@ -110,7 +97,7 @@ public class MyLinkedList {
             succ = pred.next.next;
         }else {
             succ = tail;
-            for (int i = 0; i < size-index-1; i++) {
+            for(int i = 0;i< size-index-1;i++){
                 succ = succ.prev;
             }
             pred = succ.prev.prev;
@@ -118,7 +105,6 @@ public class MyLinkedList {
 
         pred.next = succ;
         succ.prev = pred;
-
         size--;
     }
 }
