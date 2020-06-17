40124f7202d155c3c36ff5de53a9035a748568cc
lhh <henghui@youxin.cloud>
2020-06-17 14:14:49 +0800

    添加 (swagger 导出) demo

---
+++ src/main/java/com/javalearning/demo/swagger_export/Test.java
@@ -0,0 +1,42 @@
+package com.javalearning.demo.swagger_export;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class Test {
+    public static void main(String[] args) throws MalformedURLException {
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+        // swagger-ui.html页面中能找到此链接
+        Swagger2MarkupConverter.from(new URL("http://oryx-order.test3.rongxin.tech/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("D:\\Files\\youxin\\"));
+
+//        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+//                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+//                .withOutputLanguage(Language.ZH)
+//                .withPathsGroupedBy(GroupBy.TAGS)
+//                .withGeneratedExamples()
+//                .withoutInlineSchema()
+//                .build();
+//
+//        Swagger2MarkupConverter.from(new URL("http://oryx-order.test3.rongxin.tech/v2/api-docs"))
+//                .withConfig(config)
+//                .build()
+//                .toFolder(Paths.get("D:\\Files\\youxin\\"));
+    }
+}
