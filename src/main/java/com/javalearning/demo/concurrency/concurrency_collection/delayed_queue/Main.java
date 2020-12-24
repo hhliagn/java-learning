ece25c635b00ed65fd4212db57b208746e00f7b5
lhh <811467665@qq.com>
2020-12-24 16:45:40 +0800

    add delay_queue

---
+++ src/main/java/com/javalearning/demo/concurrency/concurrency_collection/delayed_queue/Main.java
@@ -0,0 +1,47 @@
+package com.javalearning.demo.concurrency.concurrency_collection.delayed_queue;
+
+import java.util.Date;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        DelayQueue<Event> queue = new DelayQueue<>();
+
+        Thread[] threads = new Thread[5];
+
+        for (int i = 0; i < threads.length; i++) {
+            Task task = new Task(i + 1, queue);
+            Thread thread = new Thread(task);
+            threads[i] = thread;
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        do {
+            int counter = 0;
+            Event event;
+            do {
+                event = queue.poll();
+                if (event != null){
+                    counter ++;
+                }
+            }while (event != null);
+
+            System.out.printf("At %s you have read %d events\n", new Date(), counter);
+            TimeUnit.MILLISECONDS.sleep(500);
+        }while (queue.size() > 0);
+    }
+}
